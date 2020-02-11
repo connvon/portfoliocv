1a4f352dc902228e77dc05f555caacfbb7f02526
connvon <60625152+connvon@users.noreply.github.com>
2020-02-11 12:26:51 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function linkedinhover() {
+  document.getElementById('linkedinicon').src='images/linkedinhover.png'
+}
+
+function linkedinout() {
+  document.getElementById('linkedinicon').src='images/linkedin.png'
+}
